72b5a9a89ac607a01b7751de5ce4c10f480a5fd5
Rubens Freire <rubensgomail@gmail.com>
2025-03-16 02:56:48 +0000

    starting string

--- string.c
+++ string.c
@@ -1,18 +1,55 @@
 #include "string.h"
 
-char	*ft_strdup(t_string *this)
+t_string	*str_dup(t_string *this)
 {
-	int		i;
-	char	*copy;
+	int			i;
+	t_string	*copy;
 
-	copy = ft_calloc(sizeof(char), (this->length + 1));
-	if (!copy)
+	copy = malloc(sizeof(t_string));
+	copy->string = ft_calloc(sizeof(char), (this->length + 1));
+	if (!copy->string)
 		return (NULL);
 	i = 0;
 	while (this->string[i] != '\0')
 	{
-		copy[i] = this->string[i];
+		copy->string[i] = this->string[i];
 		i++;
 	}
+	copy->length = this->length;
 	return (copy);
+}
+
+void	str_combine()
+{
+
+}
+
+void	str_join()
+{
+
+}
+
+void	str_equals()
+{
+
+}
+
+void	str_contains()
+{
+
+}
+
+void	str_foreach()
+{
+
+}
+
+void	str_print()
+{
+
+}
+
+void	str_destroy()
+{
+	
 }
